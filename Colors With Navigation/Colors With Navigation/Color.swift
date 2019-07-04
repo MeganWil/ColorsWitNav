b29e8d4fbdd94b476d045522770865e325330481
Megan Wilson <mlw7v3@mail.missouri.edu>
2019-07-04 02:10:01 -0500

    added color to each row

---
+++ Colors With Navigation/Colors With Navigation/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  Colors With Navigation
+//
+//  Created by Megan Wilson on 7/4/19.
+//  Copyright © 2019 Megan Wilson. All rights reserved.
+//
+
+import UIKit
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
